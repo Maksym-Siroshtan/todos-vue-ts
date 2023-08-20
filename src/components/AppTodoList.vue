91b3abede7dd330ff3e70b50442cb9910e9229d8
Maksym <siroshtan.max1991ua@gmail.com>
2023-08-20 15:29:37 +0300

    Added toggle state logic and note deletion logic

--- src/components/AppTodoList.vue
+++ src/components/AppTodoList.vue
@@ -1,18 +1,28 @@
 <template>
   <ul class="todo-list">
-    <AppTodoListItem v-for="todo in todos" :todo="todo" :key="todo.id" />
+    <AppTodoListItem
+      v-for="todo in todos"
+      :todo="todo"
+      :key="todo.id"
+      @toggle-todo="toggleTodo"
+      @delete-todo="deleteTodo"
+    />
   </ul>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { Todo } from "../types/Todo";
 import AppTodoListItem from "./AppTodoListItem.vue";
 
+interface State {
+  todos: Todo[];
+}
 export default defineComponent({
   components: {
     AppTodoListItem,
   },
-  data() {
+  data(): State {
     return {
       todos: [
         { id: 0, text: "Learn the basics of Vue", completed: true },
@@ -20,6 +30,16 @@ export default defineComponent({
         { id: 2, text: "Subscribe to the channel", completed: false },
       ],
     };
+  },
+  methods: {
+    toggleTodo(id: number) {
+      this.todos.find((todo: Todo) =>
+        todo.id === id ? (todo.completed = !todo.completed) : null
+      );
+    },
+    deleteTodo(id: number) {
+      this.todos = this.todos.filter((todo: Todo) => todo.id !== id);
+    },
   },
 });
 </script>
--- src/components/AppTodoListItem.vue
+++ src/components/AppTodoListItem.vue
@@ -1,10 +1,14 @@
 <template>
-  <li class="todo-item" :class="{ 'todo-item--done': todo.completed }">
+  <li
+    class="todo-item"
+    :class="{ 'todo-item--done': todo.completed }"
+    @click="toggleTodo"
+  >
     <div class="todo-item__status">
       <i class="bi bi-check2"></i>
     </div>
     <span class="todo-item__text">{{ todo.text }}</span>
-    <button class="todo-item__remove-button">
+    <button class="todo-item__remove-button" @click.stop="deleteTodo">
       <i class="bi bi-trash3"></i>
     </button>
   </li>
@@ -12,17 +16,26 @@
 
 <script lang="ts">
 import { defineComponent, PropType } from "vue";
+import { Todo } from "../types/Todo";
 
 export default defineComponent({
   props: {
     todo: {
       required: true,
-      type: Object as PropType<{
-        id: number;
-        text: string;
-        completed: boolean;
-      }>,
+      type: Object as PropType<Todo>,
     },
+  },
+  methods: {
+    toggleTodo() {
+      this.$emit("toggleTodo", this.todo.id);
+    },
+    deleteTodo() {
+      this.$emit("deleteTodo", this.todo.id);
+    },
+  },
+  emits: {
+    toggleTodo: (id: number) => Number.isInteger(id),
+    deleteTodo: (id: number) => Number.isInteger(id),
   },
 });
 </script>
