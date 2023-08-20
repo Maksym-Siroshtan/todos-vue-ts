1aaac733e6b5d3ec5e6d40cff4f9bbf813f590e1
Maksym <siroshtan.max1991ua@gmail.com>
2023-08-20 13:59:21 +0300

    Application markup broken down into components

---
+++ src/components/AppTodoList.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul class="todo-list">
+    <AppTodoListItem v-for="todo in todos" :todo="todo" :key="todo.id" />
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import AppTodoListItem from "./AppTodoListItem.vue";
+
+export default defineComponent({
+  components: {
+    AppTodoListItem,
+  },
+  data() {
+    return {
+      todos: [
+        { id: 0, text: "Learn the basics of Vue", completed: true },
+        { id: 1, text: "Learn the basics of Typescript", completed: false },
+        { id: 2, text: "Subscribe to the channel", completed: false },
+      ],
+    };
+  },
+});
+</script>
